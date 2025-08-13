eca4ca89831e318593db033af86c2fbdf97f2566
mogere <pmnyatichi@gmail.com>
2025-08-13 23:05:58 +0300

    feat: add Header and Footer components to layout; create new pages for Autoparts, Bodykits, Garage, and Sales

---
+++ src/app/autoparts/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Autoparts = () => {
+  return <div>Autoparts Page</div>;
+};
+
+export default Autoparts;
---
+++ src/app/garage/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Garage = () => {
+  return (
+    <div>
+      <h1>This is the garage page</h1>
+    </div>
+  );
+};
+
+export default Garage;
